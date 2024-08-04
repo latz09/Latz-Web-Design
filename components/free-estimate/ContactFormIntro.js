1f74268231e591804b7251885c92a40630700eea
latz09 <85132328+latz09@users.noreply.github.com>
2024-08-04 15:54:31 -0500

    contact

--- components/free-estimate/ContactFormIntro.js
+++ components/free-estimate/ContactFormIntro.js
@@ -4,15 +4,15 @@ import { Description, MainHeading, TopHeading } from '../utils/Typography';
 const ContactFormIntro = () => {
 	return (
 		<div className='text-center lg:text-start  p-6 text-light space-y-4'>
-			<MainHeading
-				title={`Let's Get Started`}
-				theme='text-tertiary'
-			/>
-            <SlideIn>
-			<p className='text-lg lg:text-xl font-normal leading-8 lg:leading-10 '>
-				Let's turn your ideas into an impactful website. Fill out our simple form, and we will personally reach out to discuss your vision. Together, we'll create a custom solution that aligns with your goals and brand identity.
-			</p></SlideIn>
-            
+			<MainHeading title={`Let's Get Started`} theme='text-tertiary' />
+			<SlideIn>
+				<p className='text-lg lg:text-xl font-normal leading-8 lg:leading-10 '>
+				{`	Let's turn your ideas into an impactful website. Fill out our simple
+					form, and we will personally reach out to discuss your vision.
+					Together, we'll create a custom solution that aligns with your goals
+					and brand identity.`}
+				</p>
+			</SlideIn>
 		</div>
 	);
 };
