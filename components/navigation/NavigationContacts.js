f52f160c9c32e26baf922e8950f104184619e823
latz09 <85132328+latz09@users.noreply.github.com>
2024-08-04 18:35:08 -0500

    nav link

---
+++ components/navigation/NavigationContacts.js
@@ -0,0 +1,53 @@
+import { FaFacebookF, FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa6';
+
+const NavigationContacts = () => {
+	return (
+		<div className='grid place-items-start lg:place-items-end'>
+			<div className='text-dark text-lg lg:space-y-16'>
+				<div className='text-tertiary/80  grid gap-1'>
+					<a
+						href='mailto:jordan@latzwebdesign.com'
+						className='underline underline-offset-8 mb-4'
+					>
+						jordan@latzwebdesign.com
+					</a>
+
+					<a href='tel:7156305689' className='underline underline-offset-8 decoration-tertiary/60'>
+						(715) 630-5689
+					</a>
+				</div>
+
+				<div className="hidden lg:block">
+					<SocialMediaLinks />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default NavigationContacts;
+
+export const SocialMediaLinks = () => {
+	return (
+		<div className='flex  text-light/80 text-3xl justify-between mt-4'>
+			<a
+				href='https://www.linkedin.com/in/jordan-latz-0b6b0b1b5/'
+				target='_blank'
+				rel='noreferrer'
+			>
+				<FaLinkedinIn />
+			</a>
+			<a href=' ' target='_blank' rel='noreferrer'>
+				<FaInstagram />
+			</a>
+
+			<a href=' ' target='_blank' rel='noreferrer'>
+				<FaFacebookF />
+			</a>
+			<a href=' ' target='_blank' rel='noreferrer'>
+				<FaGithub />
+			</a>
+		</div>
+	);
+};
--- components/utils/ModalOverlay.js
+++ components/utils/ModalOverlay.js
@@ -1,8 +1,11 @@
 'use client';
 
+
 import { motion } from 'framer-motion';
 import NavigationLinks from '../navigation/NavigationLinks'; // Ensure you have this component
 import NavigationLogo, { LogoGuy } from '../navigation/NavigationLogo';
+import NavigationContacts from '../navigation/NavigationContacts';
+import Link from 'next/link';
 
 const ModalOverlay = ({ onClose }) => {
 	return (
@@ -19,7 +22,9 @@ const ModalOverlay = ({ onClose }) => {
 					<LogoGuy />
 				</div>
 				<div className='flex items-center space-x-8'>
+                    <Link href='contact-latz-web-design' onClick={onClose}>
 					<div className='text-white text-xl'>{`Let's Talk`}</div>
+                    </Link>
 					<button
 						onClick={onClose}
 						className='text-xl text-light text-gray-500 hover:text-gray-700'
@@ -32,23 +37,10 @@ const ModalOverlay = ({ onClose }) => {
 			{/* Main Modal Content */}
 			<motion.div className='max-w-5xl mx-auto w-full bg-white rounded-lg p-8 relative  grid justif-center mt-8'>
 				<div className='flex flex-col gap-16 lg:gap-24 lg:flex-row lg:justify-between   '>
-				
 					<div className='flex flex-col space-y-4'>
 						<NavigationLinks onLinkClick={onClose} />
 					</div>
-
-					
-					<div className='grid place-items-start lg:place-items-end'>
-						<div className='text-dark  text-lg lg:space-y-16'>
-							<div className='lg:space-y-4 text-tertiary'>
-								<p>jordan@latzwebdesign.com</p>
-								<p>{`(715) 630-5689`}</p>
-							</div>
-							<div className='text-dark/70'>
-								<p>West Saint Paul, MN</p>
-							</div>
-						</div>
-					</div>
+					<NavigationContacts />
 				</div>
 			</motion.div>
 		</motion.div>
@@ -56,3 +48,5 @@ const ModalOverlay = ({ onClose }) => {
 };
 
 export default ModalOverlay;
+
+
