b91112f0070a986dbc161c876f63b1cc84c8cf2b
latz09 <85132328+latz09@users.noreply.github.com>
2024-08-04 18:54:29 -0500

    footer

--- components/navigation/Footer.js
+++ components/navigation/Footer.js
@@ -19,7 +19,7 @@ const Footer = () => {
             <div className=" max-w-5xl w-2/3 lg:w-1/4 opacity-60 mb-8">
                 <SocialMediaLinks   />
             </div>
-			<div className='grid place-items-center lg:flex gap-8 lg:gap-4 max-w-5xl w-full justify-evenly text-xl lg:text-2xl font-bold tracking-wider'>
+			<div className='grid place-items-center lg:flex gap-8 lg:gap-4 max-w-5xl w-full justify-evenly text-lg lg:text-2xl font-bold tracking-wider'>
 				{navigationLinks.map((link) => (
 					<Link key={link.id} href={link.href}>
 						<span>{link.title}</span>
