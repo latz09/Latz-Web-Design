6b83ae0ab6f1a124c89d2a57c2de6da201dc0811
latz09 <85132328+latz09@users.noreply.github.com>
2024-08-05 13:31:42 -0500

    contacts on form

--- components/free-estimate/ContactFormContainer.js
+++ components/free-estimate/ContactFormContainer.js
@@ -7,14 +7,39 @@ const ContactFormContainer = ({ toggleModal }) => {
 	return (
 		<div className='bg-gradient-to-b from-dark via-dark/90 to-dark pt-8 pb-32'>
 			<div className=' max-w-7xl mx-auto grid  lg:grid-cols-2 gap-8 lg:gap-2 place-items-center '>
-				<ContactFormIntro />
+				<div className='p-6 grid gap-8'>
+					<ContactFormIntro />
+					<div className='hidden lg:block'>
+						<Contacts />
+					</div>
+				</div>
 
 				<AnimateUp className='lg:w-5/6 w-full px-4 lg:px-0'>
 					<ContactForm toggleModal={toggleModal} />
 				</AnimateUp>
+				<div className="lg:hidden mt-8 text-center">
+					<Contacts />
+				</div>
 			</div>
 		</div>
 	);
 };
 
 export default ContactFormContainer;
+
+const Contacts = () => {
+	return (
+		<div className='text-tertiary  grid gap-1'>
+			<a
+				href='mailto:jordan@latzwebdesign.com'
+				className='underline underline-offset-8 mb-4'
+			>
+				jordan@latzwebdesign.com
+			</a>
+
+			<a href='tel:7156305689' className='underline underline-offset-8'>
+				(715) 630-5689
+			</a>
+		</div>
+	);
+};
--- components/free-estimate/ContactFormIntro.js
+++ components/free-estimate/ContactFormIntro.js
@@ -3,16 +3,13 @@ import { Description, MainHeading, TopHeading } from '../utils/Typography';
 
 const ContactFormIntro = () => {
 	return (
-		<div className='text-center lg:text-start  p-6 text-light space-y-4'>
+		<div className='text-center lg:text-start   text-light space-y-4'>
 			<MainHeading title={`Let's Get Started`} theme='text-tertiary' />
-		
-				<p className='text-lg lg:text-xl font-normal leading-8 lg:leading-10 '>
+
+			<p className='text-lg lg:text-xl font-normal leading-8 lg:leading-10 '>
 				{`	Let's turn your ideas into an impactful website. Fill out our simple
-					form, and we will personally reach out to discuss your vision.
-					Together, we'll create a custom solution that aligns with your goals
-					and brand identity.`}
-				</p>
-			
+					form, and we will personally reach out to discuss your vision.`}
+			</p>
 		</div>
 	);
 };
