1622b8e0dbba6a842ef34918cd166be27348a336
latz09 <85132328+latz09@users.noreply.github.com>
2024-08-08 15:28:08 -0500

    d

--- components/RecentClients/RecentClientCard.js
+++ components/RecentClients/RecentClientCard.js
@@ -27,15 +27,19 @@ const RecentClientCard = ({ data }) => {
 						style={{ objectFit: 'cover', objectPosition: 'center' }}
 						className='rounded-sm'
 					/>
-					<div className='absolute inset-0 bg-dark/40 transition-colors duration-500 ease-in-out md:group-hover:bg-dark/30 flex flex-col justify-between text-light '>
-						<div className='flex flex-col bg-dark/5 transition-colors duration-500 ease-in-out md:group-hover:bg-dark/100'>
-							<h3 className='text-xl lg:text-2xl font-bold px-6 py-2 bg-dark/80 text-light   shadow-lg  '>{companyName}</h3>
+					<div className='absolute inset-0 bg-gradient-to-b from-dark/0 via-primary/20 to-dark transition-colors duration-500 ease-in-out md:group-hover:bg-dark/30 flex flex-col justify-between text-light'>
+						<div className='flex justify-between items-center px-6 py-2 bg-dark/90'>
+							<h3 className='text-xl font-bold text-light shadow-lg'>
+								{companyName}
+							</h3>
+							<div className='text-sm lg:text-base lg:font-semibold tracking-wider flex items-center'>
+								<span className='md:group-hover:text-tertiary'>Visit Site</span>
+								<FaLongArrowAltRight className='inline-block text-tertiary ml-2 transform transition-transform duration-500 ease-in-out group-hover:translate-x-2 text-lg lg:text-2xl ' />
+							</div>
 						</div>
-						<div className='flex flex-col bg-dark/40 px-6 pt-4  transition-colors duration-500 ease-in-out md:group-hover:bg-light/80 md:group-hover:text-dark'>
-							<div className='text-sm lg:text-base md:group-hover:font-bold'>{websiteDescription}</div>
-							<div className='text-sm lg:text-base place-self-end py-3 lg:py-2 lg:font-semibold tracking-wider flex items-center '>
-								<span className="md:group-hover:text-primary">Visit Site</span>
-								<FaLongArrowAltRight className='inline-block text-tertiary ml-2 transform transition-transform duration-500 ease-in-out group-hover:translate-x-2 text-lg lg:text-2xl group-hover:text-primary' />
+						<div className='flex flex-col bg-dark/30 px-6 py-4 transition-colors duration-500 ease-in-out md:group-hover:bg-light/80 md:group-hover:text-dark '>
+							<div className='text-sm lg:text-base  md:group-hover:font-bold'>
+								{websiteDescription}
 							</div>
 						</div>
 					</div>
