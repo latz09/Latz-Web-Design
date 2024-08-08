8bd18203d1b2d6d8f0069fbe8994d3c038ee730c
latz09 <85132328+latz09@users.noreply.github.com>
2024-08-08 15:09:20 -0500

    client card

--- components/RecentClients/RecentClientCard.js
+++ components/RecentClients/RecentClientCard.js
@@ -27,12 +27,14 @@ const RecentClientCard = ({ data }) => {
 						style={{ objectFit: 'cover', objectPosition: 'center' }}
 						className='rounded-sm'
 					/>
-					<div className='absolute inset-0 bg-dark/70 transition-colors duration-500 ease-in-out md:group-hover:bg-dark/30 flex flex-col justify-end text-light '>
-						<div className='flex flex-col bg-dark/5 px-6 pt-4 transition-colors duration-500 ease-in-out md:group-hover:bg-dark/100'>
-							<h3 className='text-lg lg:text-2xl font-bold'>{companyName}</h3>
-							<div className='text-sm lg:text-base'>{websiteDescription}</div>
-							<div className='text-sm lg:text-base place-self-end py-3 lg:py-2 lg:font-semibold tracking-wider flex items-center'>
-								<span>Visit Site</span>
+					<div className='absolute inset-0 bg-dark/40 transition-colors duration-500 ease-in-out md:group-hover:bg-dark/30 flex flex-col justify-between text-light '>
+						<div className='flex flex-col bg-dark/5 transition-colors duration-500 ease-in-out md:group-hover:bg-dark/100'>
+							<h3 className='text-xl lg:text-2xl font-bold px-6 py-2 bg-light/80 text-dark/80 md:group-hover:bg-dark md:group-hover:text-light  shadow-lg  '>{companyName}</h3>
+						</div>
+						<div className='flex flex-col bg-dark/40 px-6 pt-4  transition-colors duration-500 ease-in-out md:group-hover:bg-light/80 md:group-hover:text-dark'>
+							<div className='text-sm lg:text-base md:group-hover:font-bold'>{websiteDescription}</div>
+							<div className='text-sm lg:text-base place-self-end py-3 lg:py-2 lg:font-semibold tracking-wider flex items-center '>
+								<span className="md:group-hover:text-primary">Visit Site</span>
 								<FaLongArrowAltRight className='inline-block text-tertiary ml-2 transform transition-transform duration-500 ease-in-out group-hover:translate-x-2 text-lg lg:text-2xl group-hover:text-primary' />
 							</div>
 						</div>
