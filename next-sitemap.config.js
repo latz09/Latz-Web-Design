ec271ecd9317cefa86b586620a3992becf4637b1
latz09 <85132328+latz09@users.noreply.github.com>
2024-08-09 08:13:29 -0500

    new sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://latz-web-design.vercel.app', // Replace with your site's URL
+    siteUrl: 'https://www.latzwebdesign.com', // Replace with your site's URL
     generateRobotsTxt: true, // (optional) Generate a robots.txt file
     // Exclude specific routes (optional)
     exclude: ['/admin/*'],
--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,24 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-	  remotePatterns: [
-		{
-		  protocol: 'https',
-		  hostname: 'cdn.sanity.io',
-		  pathname: '/**',
-		},
-	  ],
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cdn.sanity.io',
+				pathname: '/**',
+			},
+		],
 	},
-  };
-  
-  export default nextConfig;
-  
+	async redirects() {
+		return [
+			{
+				source: '/articles/:slug', // Old article URLs
+				destination: '/', // Redirect to the homepage or another relevant page
+				permanent: true, // Permanent redirect (301 status)
+			},
+			// Add more redirects as needed
+		];
+	},
+};
+
+export default nextConfig;
