20e596f77a0f3dbaca78badbaf860c198d33bda0
latz09 <85132328+latz09@users.noreply.github.com>
2024-08-14 17:11:18 -0500

    client card

--- components/RecentClients/RecentClientCard.js
+++ components/RecentClients/RecentClientCard.js
@@ -3,50 +3,46 @@ import { FaLongArrowAltRight } from 'react-icons/fa';
 import AnimateUp from '../utils/animations/AnimateUp';
 
 const RecentClientCard = ({ data }) => {
-	const {
-		websiteImageUrl,
-		clientLogoUrl,
-		companyName,
-		companyUrl,
-		websiteDescription,
-	} = data;
+  const { websiteImageUrl, companyName, companyUrl, websiteDescription } = data;
 
-	return (
-		<AnimateUp>
-			<a
-				href={companyUrl}
-				target='_blank'
-				rel='noopener noreferrer'
-				className='block group relative w-full h-64 lg:h-80 overflow-hidden transform transition-all duration-700 md:hover:scale-95 rounded-sm'
-			>
-				<div className='relative w-full h-full'>
-					<Image
-						src={websiteImageUrl}
-						alt={`${companyName} Website`}
-						fill
-						style={{ objectFit: 'cover', objectPosition: 'center' }}
-						className='rounded-sm'
-					/>
-					<div className='absolute inset-0 bg-gradient-to-b from-dark/0 via-primary/20 to-dark transition-colors duration-500 ease-in-out md:group-hover:bg-dark/30 flex flex-col justify-between text-light'>
-						<div className='flex justify-between items-center px-6 py-3 bg-light'>
-							<h3 className='text-xl font-bold text-dark '>
-								{companyName}
-							</h3>
-							<div className='text-sm lg:text-base lg:font-semibold  flex items-center'>
-								<span className=' text-dark font-bold'>Visit Site</span>
-								<FaLongArrowAltRight className='inline-block text-primary ml-2 transform transition-transform duration-500 ease-in-out group-hover:translate-x-2 text-lg lg:text-2xl ' />
-							</div>
-						</div>
-						<div className='flex flex-col bg-dark/30 px-6 py-4 transition-colors duration-500 ease-in-out md:group-hover:bg-light/80 md:group-hover:text-dark '>
-							<div className='text-sm lg:text-base  md:group-hover:font-bold'>
-								{websiteDescription}
-							</div>
-						</div>
-					</div>
-				</div>
-			</a>
-		</AnimateUp>
-	);
+  return (
+    <AnimateUp>
+      <a
+        href={companyUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-sm lg:text-base lg:font-semibold flex items-center"
+      >
+        <div className="group grid gap-4 relative w-full h-auto overflow-hidden transform transition-all duration-700 rounded-sm">
+          {/* Header Section */}
+          <div className="flex justify-between items-center py-3 bg-dark text-light border-b border-primary mx-4">
+            <h3 className="text-xl font-bold text-tertiary md:text-light md:group-hover:text-tertiary">{companyName}</h3>
+            <div>
+              <span className="font-bold">Visit Site</span>
+              <FaLongArrowAltRight className="inline-block text-primary ml-2 transform transition-transform duration-500 ease-in-out group-hover:translate-x-2 text-lg lg:text-2xl" />
+            </div>
+          </div>
+
+          {/* Image Section */}
+          <div className="relative w-full h-80 md:group-hover:scale-95 transition duration-700">
+            <Image
+              src={websiteImageUrl}
+              alt={`${companyName} Website`}
+              fill
+              style={{ objectFit: 'cover', objectPosition: 'center' }}
+              className="lg:rounded-xl"
+            />
+            {/* Description Section on the Image */}
+            <div className="absolute bottom-0 left-0 right-0 bg-dark/80 text-light text-center p-4 transition duration-1000 md:group-hover:bg-dark">
+              <p className="italic text-base lg:text-lg md:group-hover:font-bol font-normal ">
+                {websiteDescription}
+              </p>
+            </div>
+          </div>
+        </div>
+      </a>
+    </AnimateUp>
+  );
 };
 
 export default RecentClientCard;
