2a236f5d8916143d2eef82a3149f0a6754ea8e22
latz09 <85132328+latz09@users.noreply.github.com>
2024-08-26 16:37:47 -0500

    featured articles

--- components/articles/FeaturedArticle.js
+++ components/articles/FeaturedArticle.js
@@ -5,7 +5,7 @@ import { Description, MainHeading, TopHeading } from '../utils/Typography';
 const FeaturedArticle = async ({ featuredArticle, synopsis }) => {
 	return (
 		<Link href={`/articles/${featuredArticle.slug.current}`}>
-			<div className='relative w-full h-[33rem] group '>
+			<div className='relative w-full h-[33rem] group'>
 				{/* Background Image */}
 				<Image
 					src={featuredArticle.articleImage.asset.url}
@@ -16,29 +16,29 @@ const FeaturedArticle = async ({ featuredArticle, synopsis }) => {
 				/>
 
 				{/* Overlay */}
-				<div className='absolute inset-0 bg-gradient-to-b from-dark/95 via-dark/70 to-dark/90 z-10'></div>
+				<div className='absolute inset-0 bg-gradient-to-b from-dark/95 via-dark/80 to-dark/40 z-10'></div>
 
-				{/* Title, Subtitle, Button, and Synopsis */}
-				<div className='absolute inset-0 z-20 flex flex-col justify-between p-4'>
-					{/* Title and Subtitle at the Top */}
-					<div className='grid place-items-center gap-2 pt-8 text-center'>
+				{/* Title, Subtitle, Synopsis, and Button Centered */}
+				<div className='absolute inset-0 z-20 flex flex-col justify-center items-center text-center space-y-6 p-4'>
+					{/* Title and Subtitle */}
+					<div className='space-y-2'>
 						<MainHeading title={featuredArticle.title} theme='text-light ' />
 						<TopHeading title={featuredArticle.subTitle} theme='text-light ' />
 					</div>
 
-					{/* Button in the Center */}
-					<div className='flex justify-center my-4'>
+					{/* Synopsis */}
+					{synopsis && (
+						<div className='text-light lg:text-2xl lg:w-5/6 leading-7 lg:leading-9'>
+							<p>{synopsis}</p>
+						</div>
+					)}
+
+					{/* Button */}
+					<div>
 						<button className='bg-tertiary text-dark py-2 px-12 rounded-sm font-semibold group-hover:bg-primary group-hover:text-light group-hover:rotate-2 group-hover:scale-110 transition duration-700 text-xl lg:text-3xl'>
 							Read Full Article
 						</button>
 					</div>
-
-					{/* Synopsis at the Bottom */}
-					{synopsis && (
-						<div className='flex items-end justify-center pb-4 text-light  lg:text-2xl text-center lg:w-5/6 mx-auto leading-7 lg:leading-9 '>
-							<p>{synopsis}</p>
-						</div>
-					)}
 				</div>
 			</div>
 		</Link>
