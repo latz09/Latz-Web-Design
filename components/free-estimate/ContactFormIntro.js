6594217eb54904e8410058363665dce35c559807
latz09 <85132328+latz09@users.noreply.github.com>
2024-10-01 15:15:43 -0500

    biz app updates

--- components/free-estimate/ContactFormIntro.js
+++ components/free-estimate/ContactFormIntro.js
@@ -7,10 +7,10 @@ const ContactFormIntro = () => {
 			<MainHeading title={`Let's Get Started`} theme='text-dark' />
 			<div className='text-lg lg:text-xl font-normal space-y-2'>
 				<p className='leading-8 lg:leading-10'>
-					{`Ready to elevate your business? Whether you need a custom website that stands out or want to streamline your operations with our CRM & Automation services, we're here to help. `}
+					{`Ready to elevate your business? Whether you need a custom website that stands out or want to streamline your operations with our CRM & Automation services, we are here to help. `}
 				</p>
 				<p className='leading-8 lg:leading-10 font-bold'>
-					{`Fill out the form, and we'll reach out to discuss how we can bring
+					{`Fill out the form, and we will reach out to discuss how we can bring
 					your vision to life.`}
 				</p>
 			</div>
