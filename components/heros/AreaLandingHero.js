012d7ca1d5514aba77ab5d02d7eb110db834cde9
latz09 <85132328+latz09@users.noreply.github.com>
2024-12-17 13:01:40 -0600

    larger mobile heading font

--- components/heros/AreaLandingHero.js
+++ components/heros/AreaLandingHero.js
@@ -39,11 +39,11 @@ const CONTAINER_CLASSES =
 	'relative h-[90vh] xl:h-[100vh]  overflow-hidden bg-dark text-light';
 const INNER_CONTAINER_CLASSES =
 	'relative z-10 flex flex-col justify-center items-center h-full max-w-5xl px-4 mx-auto text-center';
-const HEADING_CLASSES = 'grid gap-4 space-y-4';
+const HEADING_CLASSES = 'grid gap-4 lg:space-y-4';
 const TOP_HEADING_CLASSES =
 	'text-lg lg:text-2xl font-light uppercase tracking-widest text-tertiary';
 const MAIN_HEADING_CLASSES =
-	'text-3xl lg:text-7xl font-extrabold text-white leading-tight';
+	'text-4xl lg:text-7xl text-center font-extrabold text-white leading-tight';
 const SUB_HEADING_CLASSES = 'text-md lg:text-2xl text-light/80';
 const BUTTON_CLASSES = 'transition duration-700 hover:scale-95 block text-center p-3 lg:p-4 lg:w-1/2 mx-auto text-lg lg:text-2xl  rounded-full font-bold bg-tertiary text-dark hover:bg-tertiary/0 hover:text-tertiary hover:border';
 
