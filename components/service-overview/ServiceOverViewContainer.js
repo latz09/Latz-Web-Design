1d2c6ce2c21c13ddc86a1364f7829d80542dcce3
latz09 <85132328+latz09@users.noreply.github.com>
2024-12-18 10:59:40 -0600

    service overview animation

--- components/service-overview/ServiceOverViewContainer.js
+++ components/service-overview/ServiceOverViewContainer.js
@@ -1,67 +1,25 @@
 import React from 'react';
-import {
-	FaCode,
-	FaCog,
-	FaRocket,
-	FaSearch,
-	FaLaptop,
-	FaCloud,
-} from 'react-icons/fa'; // Import icons
 import { fetchData } from '@/lib/sanity/sanityFetch';
 import { FETCH_SERVICES_OVERVIEW as query } from '@/data/queries/services/FETCH_SERVICE_OVERVIEW_DATA_QUERY';
-import { Description, MainHeading, TopHeading } from '../utils/Typography';
-import AnimateUp from '../utils/animations/AnimateUp';
+import { MainHeading, TopHeading } from '../utils/Typography';
+import ServicesOverview from './ServicesOverview';
 
-// Tailwind CSS classes
 const SERVICES_CONTAINER_CLASSES =
 	'bg-gradient-to-b from-light via-primary/10 to-light py-16 lg:py-24 px-4 lg:px-8 grid gap-8 mt-2 ';
-const SERVICES_WRAPPER_CLASSES =
-	'max-w-7xl mx-auto grid gap-x-8 gap-y-16 lg:gap-x-16 lg:gap-y-24 md:grid-cols-2 lg:grid-cols-3 text-center ';
-const SERVICE_CARD_CLASSES = 'flex flex-col  items-center gap-4 group';
-const ICON_CLASSES = 'text-7xl lg:text-6xl text-primary group-hover:scale-110 group-hover:text-dark group-hover:rotate-12 transition duration-500';  
-
-const iconMap = {
-	FaLaptop: FaLaptop,
-	FaCode: FaCode,
-	FaCog: FaCog,
-	FaRocket: FaRocket,
-	FaSearch: FaSearch,
-	FaCloud: FaCloud,
-};
 
 const ServicesOverviewContainer = async () => {
-	// Fetch data from Sanity
+	// Fetch data
 	const data = await fetchData(query);
 	const { heading, subheading, services } = data || {};
 
 	return (
 		<section className={SERVICES_CONTAINER_CLASSES}>
-			{/* Section Heading */}
 			<div className='text-center mb-12 space-y-4'>
 				<MainHeading title={heading} />
-
 				<TopHeading title={subheading} />
 			</div>
-
-			{/* Services Grid */}
-			<div className={SERVICES_WRAPPER_CLASSES}>
-				{services?.map((service, index) => {
-					const Icon = iconMap[service.icon] || FaLaptop; // Default icon fallback
-					return (
-						<AnimateUp key={index}>
-							<div className={SERVICE_CARD_CLASSES}>
-								<div className=''>
-									<Icon className={ICON_CLASSES} />
-								</div>
-
-								<TopHeading title={service.title} theme='group-hover:text-primary transition duration-500 ' />
-
-								<Description description={service.description} theme='group-hover:font-bold transition duration-500'/>
-							</div>
-						</AnimateUp>
-					);
-				})}
-			</div>
+			<div className="">
+			<ServicesOverview services={services} /></div>
 		</section>
 	);
 };
---
+++ components/service-overview/ServicesOverview.js
@@ -0,0 +1,70 @@
+'use client';
+
+import React from 'react';
+import {
+	FaCode,
+	FaCog,
+	FaRocket,
+	FaSearch,
+	FaLaptop,
+	FaCloud,
+} from 'react-icons/fa';
+import { Description, MainHeading, TopHeading } from '../utils/Typography';
+import AnimateUp from '../utils/animations/AnimateUp';
+import { motion } from 'framer-motion';
+
+const SERVICES_WRAPPER_CLASSES =
+	'max-w-7xl mx-auto grid gap-x-8 gap-y-16 lg:gap-x-16 lg:gap-y-24 md:grid-cols-2 lg:grid-cols-3 text-center ';
+const SERVICE_CARD_CLASSES =
+	'flex flex-col items-center gap-4 transition duration-500';
+
+const iconMap = {
+	FaLaptop: FaLaptop,
+	FaCode: FaCode,
+	FaCog: FaCog,
+	FaRocket: FaRocket,
+	FaSearch: FaSearch,
+	FaCloud: FaCloud,
+};
+
+const ServicesOverview = ({ services }) => {
+	return (
+		<div className={SERVICES_WRAPPER_CLASSES}>
+			{services?.map((service, index) => {
+				const Icon = iconMap[service.icon] || FaLaptop;
+
+				return (
+					<AnimateUp key={index}>
+						<div className={SERVICE_CARD_CLASSES}>
+							<motion.div
+								initial={{ scale: 0.8 }}
+								whileInView={{ scale: 1, rotate: 12 }}
+								transition={{ duration: 1 }}
+							>
+								<Icon className='text-7xl lg:text-6xl text-primary mb-2' />
+							</motion.div>
+							<motion.div
+								initial={{ scale: 0.8 }}
+								whileInView={{ scale: 1 }}
+								transition={{ duration: 1 }}
+							>
+								<h2 className='text-xl lg:text-2xl font-bold uppercase'>
+									{service.title}
+								</h2>
+							</motion.div>
+							<motion.div
+								initial={{ scale: 0.8, rotate: 4, opacity: .6 }}
+								whileInView={{ scale: 1, rotate: 0, opacity: 1 }}
+								transition={{ duration: 1 }}
+							>
+								<Description description={service.description} theme='' />
+							</motion.div>
+						</div>
+					</AnimateUp>
+				);
+			})}
+		</div>
+	);
+};
+
+export default ServicesOverview;
