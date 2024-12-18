add07c2d33c187fc907c217bbb008a9fe820b366
latz09 <85132328+latz09@users.noreply.github.com>
2024-12-18 14:59:07 -0600

    fda

--- components/utils/ChatPointer.js
+++ components/utils/ChatPointer.js
@@ -22,7 +22,7 @@ const ChatPointer = () => {
 
     return (
         <motion.div
-            className="fixed bottom-5 right-20 bg-light border border-primary/30 text-primary px-3 py-2 rounded-full shadow-lg z-[999999] flex items-center gap-3 tracking-wider  text-xs font-bold 2xl:text-base "
+            className="fixed bottom-5 right-20 bg-light border border-primary/30 text-primary px-3 py-2 rounded-lg shadow-lg z-[999999] flex items-center gap-3 tracking-wider  text-sm font-bold 2xl:text-base "
             initial="hidden"
             animate="visible" // Always animate to 'visible'
             variants={containerVariants}
