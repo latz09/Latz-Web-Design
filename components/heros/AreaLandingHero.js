770b1aa91a858ab43820bd000db3509c87a357c9
latz09 <85132328+latz09@users.noreply.github.com>
2025-01-09 15:14:33 -0600

    home ctas smaller

--- components/heros/AreaLandingHero.js
+++ components/heros/AreaLandingHero.js
@@ -47,11 +47,11 @@ const CONTAINER_CLASSES =
 	'relative h-[90vh] xl:h-[100vh] overflow-hidden bg-dark text-light  shadow-xl shadow-tertiary/30';
 const INNER_CONTAINER_CLASSES =
 	'relative z-10 flex flex-col justify-center items-center h-full max-w-5xl px-4 mx-auto text-center';
-const HEADING_CLASSES = 'grid gap-4 lg:space-y-4';
+const HEADING_CLASSES = 'grid gap-4 lg:space-y-4 ';
 const TOP_HEADING_CLASSES =
 	'text-lg lg:text-2xl font-light uppercase tracking-widest text-tertiary';
 const MAIN_HEADING_CLASSES =
-	'text-3xl lg:text-7xl text-center font-extrabold text-white tracking-wide';
+	'text-3xl lg:text-7xl text-center font-black text-white tracking-wide';
 const SUB_HEADING_CLASSES = 'text-md lg:text-2xl text-light/80';
 
 // Component
@@ -98,7 +98,7 @@ const AreaLandingHero = ({ heading, subHeading, topHeading }) => {
 						<div className='w-full grid lg:grid-cols-2 gap-8 mt-8'>
 							<Link
 								href='/contact-latz-web-design'
-								className='w-full rounded-full mx-auto bg-tertiary text-dark py-4 grid place-items-center lg:text-2xl text-xl font-bold shdow-lg shadow-tertiary/30 hover:scale-95 transition duration-500'
+								className='w-full rounded-full mx-auto bg-tertiary text-dark py-4 grid place-items-center lg:text-2xl text-lg font-bold shdow-lg shadow-tertiary/30 hover:scale-95 transition duration-500'
 							>
 								<motion.span
 									className=''
@@ -111,7 +111,7 @@ const AreaLandingHero = ({ heading, subHeading, topHeading }) => {
 
 							<Link
 								href='/free-business-report'
-								className='w-full mx-auto border border-tertiary/20 text-tertiary0 rounded-full py-4 grid text-xl lg:text-2xl  place-items-center shadow-lg shadow-tertiary/20 hover:shadow-tertiary/50 hover:border-tertiary/50 transition duration-500 '
+								className='w-full mx-auto border border-tertiary/20 text-tertiary0 rounded-full py-4 grid text-lg lg:text-2xl  place-items-center shadow-lg shadow-tertiary/20 hover:shadow-tertiary/50 hover:border-tertiary/50 transition duration-500 '
 							>
 								<motion.span
 									className=''
