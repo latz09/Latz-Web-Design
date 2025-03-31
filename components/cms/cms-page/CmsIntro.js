d3ef6e7e08d26d448e3b937f82b2d263a20dd0a1
latz09 <85132328+latz09@users.noreply.github.com>
2025-03-31 17:18:54 -0500

    fda

--- components/cms/cms-page/CmsIntro.js
+++ components/cms/cms-page/CmsIntro.js
@@ -3,7 +3,7 @@ import { Description, MainHeading } from "@/components/utils/Typography";
 const CmsIntro = ({ title, successStory }) => {
 	return (
 		<div className='max-w-4xl mx-auto grid place-items-center gap-4 px-8 lg:px-0'>
-			<MainHeading title={title} theme='text-tertiary'/>
+			<MainHeading title={title} theme='text-tertiary text-center'/>
 
 			<Description
 				description={successStory}
