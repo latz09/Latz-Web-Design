6b14b7043a72fc36ddec82840220dd0eae3d1a78
latz09 <85132328+latz09@users.noreply.github.com>
2025-04-08 12:03:29 -0500

    more opacity nofees

--- components/utils/animations/PageContainer.js
+++ components/utils/animations/PageContainer.js
@@ -8,7 +8,7 @@ const PageContainer = ({ children, className }) => {
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1 }}
 				exit={{ opacity: 0 }}
-				transition={{ duration: 1.1 }}
+				transition={{ duration: 1.35 }}
 				className={`grid gap-24 ${className}`}
 			>
 				{children}
